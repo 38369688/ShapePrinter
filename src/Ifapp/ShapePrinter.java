9ba714f56c962c1bcd4536f257d492515497b35e
38369688 <edwardtsai0010@gmail.com>
2015-10-19 10:53:01 -0700

    check in

---
+++ src/Ifapp/ShapePrinter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ifapp;
+
+/**
+ *
+ * @author user
+ */
+public class ShapePrinter {
+
+    public static void printTriangle(int size) {
+        for (int line = 1; line <= 17; line++) {
+            System.out.print("*");
+        }
+    }
+
+}
